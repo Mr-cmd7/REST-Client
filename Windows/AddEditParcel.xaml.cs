866b2112c8598116df2fe913b72095b5907c7b3a
eshhus <ak7527203@gmail.com>
2024-02-22 13:41:05 +0200

    Update TariffWindow Посмотреть Json Там ошибка

---
+++ Windows/AddEditParcel.xaml.cs
@@ -0,0 +1,78 @@
+﻿using REST_Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace REST_Client.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditParcel.xaml
+    /// </summary>
+    public partial class AddEditParcel : Window
+    {
+        private Responce responce;
+        private HttpClient client;
+        private List<Parcel>? list;
+        public AddEditParcel(Responce responce)
+        {
+            InitializeComponent();
+            this.responce = responce;
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + responce.access_token);
+            Load_Codes();
+            Task.Run(() => Load());
+         
+        }
+        private async void Load_Codes()
+        {
+            List<Tariff>? list = await client.GetFromJsonAsync<List<Tariff>>("http://localhost:5174/api/tariff");
+            foreach(var i in list)
+            {
+                cbDepartureCode.Items.Add(i.DepartureCode);
+            }
+        }
+
+        private async Task Load()
+        {
+          
+        }
+
+        private void cbMarka_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void tbDiscounr_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+        public string SenderFullName
+        {
+            get
+            {
+                return FIO.Text;
+            }
+            set
+            {
+                FIO.Text = value;
+            }
+        }
+        public string DepartureCode
+        {
+            get;
+            set;
+        }
+    }
+}
--- Windows/TariffWindow.xaml.cs
+++ Windows/TariffWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using REST_Client.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,80 +22,81 @@ namespace REST_Client.Windows
     /// <summary>
     /// Логика взаимодействия для TariffWindow.xaml
     /// </summary>
+    
     public partial class TariffWindow : Window
     {
-        private MainWindow parent;
         private Responce response;
         private HttpClient client;
-        private Tariff? tariff;
+        private Tariff? list;
 
-        public TariffWindow(Responce response, MainWindow autorize)
+        public TariffWindow(Responce response,MainWindow mw)
         {
             InitializeComponent();
-            parent = autorize;
             this.response = response;
             client = new HttpClient();
             client.DefaultRequestHeaders.Add("Authorization", "Bearer " + response.access_token);
             Task.Run(() => Load());
 
         }
-
-        private async void Load()
+        private async Task Load()
         {
-            //List<Sales>? list = await client.GetFromJsonAsync<List<Sales>>("http://localhost:5174/api/sales");
-            //foreach (Sales i in list!)
-            //{
-            //    i.PriceList = await client.GetFromJsonAsync<PriceList>("http://localhost:5174/api/pricelist/" + i.PriceList_Id);
-            //}
-            //Dispatcher.Invoke(() =>
-            //{
-            //    SalesList.ItemsSource = null;
-            //    SalesList.Items.Clear();
-            //    SalesList.ItemsSource = list;
-            //});
-
-            List<Tariff>? list = await client.GetFromJsonAsync <List<Tariff>>("http://localhost:5247/api/tariff");
+            //HFCVJNHTNM
+            List<Tariff>? list = await client.GetFromJsonAsync<List<Tariff>>("http://localhost:7152/api/tariff");
             Dispatcher.Invoke(() =>
             {
-                TariffList.ItemsSource = null;
-                TariffList.Items.Clear();
-                TariffList.ItemsSource = list;
+                ListPrice.ItemsSource = null;
+                ListPrice.Items.Clear();
+                ListPrice.ItemsSource = list;
             });
         }
-
-        private void Window_Closed(object sender, EventArgs e)
+        private async Task Save()
         {
-            parent.Close();
+            Tariff tariif = new Tariff
+            {
+                DepartureCode = DepartureCode.Text,
+                DepartureName = DepartureName.Text,
+                PricePerWeightUnit = decimal.Parse(PricePerWeightUnit.Text)
+            };
+            JsonContent content = JsonContent.Create(tariif);
+            using var response = await client.PostAsync("http://localhost:7152/api/tariff", content);
+            string responseText = await response.Content.ReadAsStringAsync();
+            await Load();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Save();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async Task Edit()
         {
-
+            list!.DepartureCode = DepartureCode.Text;
+            list!.DepartureName = DepartureName.Text;
+            list!.PricePerWeightUnit = decimal.Parse(PricePerWeightUnit.Text);
+            JsonContent content = JsonContent.Create(list);
+            using var response = await client.PutAsync("http://localhost:7152/api/tariff", content);
+            string responseText = await response.Content.ReadAsStringAsync();
+            await Load();
         }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private async void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            await Edit();
         }
-        //private async Task Save(Tariff tariff)
-        //{
-        //    JsonContent content = JsonContent.Create(tariff);
-        //    using var response = await client.PostAsync("http://localhost:5247/api/tariff", content);
-        //    string responseText = await response.Content.ReadAsStringAsync();
-        //    await Load();
-        //}
-        //private async Task Delete()
-        //{
-        //    using var response = await client.DeleteAsync("http://localhost:5247/api/tariff" + tariff?.Id);
-        //    string responseText = await response.Content.ReadAsStringAsync();
-        //    await Load();
-        //}
-        //private async Task Edit(Tariff tariff)
-        //{
-        //    JsonContent content = JsonContent.Create(tariff);
-        //    using var response = await client.PutAsync("http://localhost:5247/api/tariff", content);
-        //    string responseText = await response.Content.ReadAsStringAsync();
-        //    await Load();
-        //}
+        private async Task Delete()
+        {
+            using var response = await client.DeleteAsync("http://localhost:7152/api/tariff/" + list?.Id);
+            string responseText = await response.Content.ReadAsStringAsync();
+            await Load();
+        }
+        private async void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            await Delete();
+        }
+        private void ListTariff_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            list = ListPrice.SelectedItem as Tariff;
+            DepartureCode.Text = list?.DepartureCode;
+            DepartureName.Text = list?.DepartureName;
+            PricePerWeightUnit.Text = list?.PricePerWeightUnit.ToString();
+        }
     }
 }
